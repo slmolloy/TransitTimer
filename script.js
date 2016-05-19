206aebd855e3dcf07c54ea1dbb34555f5c9d2386
Scott Molloy <scott.l.molloy@gmail.com>
2016-05-18 22:50:43 -0700

    Adding PlaceServices and nearbySearch feature

--- script.js
+++ script.js
@@ -13,11 +13,28 @@ function initMap() {
 
   var options = {
     bounds: defaultBounds,
-    types: ['transit_station']
+    type: 'transit_station'
   };
 
   var input = document.getElementById('pac-input');
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   var searchBox = new google.maps.places.SearchBox(input, options);
+
+  var request = {
+    bounds: defaultBounds,
+    type: 'transit_station',
+    keyword: 'max'
+  };
+
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    console.log(results);
+  } else {
+    console.log("Error");
+  }
 }
