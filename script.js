6bf920ea26eb02319268b7467884379fed17dcf2
Scott Molloy <scott.l.molloy@gmail.com>
2016-05-19 03:50:22 -0700

    Find trimet schedule time and display when clicked
    Use the Google places api lat/lng values to search Trimet API for stops
    in the area. Try to match the rigth stop by comparing the Google
    place's name with the Trimet name data. If a stop isn't found display an
    error, otherwise query Trimet API for arrivals info. Display arrivals
    info grouped by type.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var map;
 var infoWindow;
 var portland = {lat: 45.519, lng: -122.679};
 var searchBound = .01;
+var radius = 1000;
 
 function initMap() {
   var defaultBounds = new google.maps.LatLngBounds(
@@ -13,7 +14,7 @@ function initMap() {
   };
   var nearbySearchOptions = {
     location: portland,
-    radius: 1000,
+    radius: radius,
     type: ['transit_station'],
     keyword: 'max'
   };
@@ -47,37 +48,140 @@ function nearbySearchCallback(results, status) {
 }
 
 function createMarker(place) {
-  var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
   });
 
-  //marker.addListener('click', placeDetailsCallback);
   google.maps.event.addListener(marker, 'click', function() {
     var request = {
       placeId: place.place_id
     };
     var service = new google.maps.places.PlacesService(map);
-    //service.getDetails(request, placeDetailsCallback);
+
     service.getDetails(request, function(place, status) {
       if (status == google.maps.places.PlacesServiceStatus.OK) {
-        var lat = place.geometry.location.lat();
-        var lng = place.geometry.location.lng();
-        infoWindow.setContent(place.name + ' lat:' + lat + ' lng:' + lng);
-        infoWindow.open(map, marker);
-
-        var directionsService = new google.maps.DirectionsService;
-        directionsService.route({
-          origin: new google.maps.LatLng(portland.lat, portland.lng),
-          destination: new google.maps.LatLng(portland.lat, portland.lng + .08),
-          travelMode: google.maps.TravelMode.TRANSIT
-        }, function(response, status) {
-          if (status == google.maps.DirectionsStatus.OK) {
-            console.log(response);
-          }
-        })
+        showArrivals(place, map, marker);
       }
     });
   });
+}
+
+function showArrivals(place, map, marker) {
+  var lat = place.geometry.location.lat();
+  var lng = place.geometry.location.lng();
+  var stopSearchRadius = 200;
+
+  // Make call to trimet API with something like:
+  // https://developer.trimet.org/ws/V1/stops?appID=188EB70D57290068482857DDD&ll=-122.679475,45.519253&meters=1000&json=true
+  // Search results for resultSet.location[].desc = place.name
+  // Get locid from the location.
+  var req = new XMLHttpRequest();
+  req.open('GET', buildStopsUrl(lat, lng, stopSearchRadius), true);
+
+  req.addEventListener('load', function() {
+    if (req.status >= 200 && req.status < 400) {
+      var response = JSON.parse(req.responseText);
+      var locationDesc;
+      for (var i = 0; i < response.resultSet.location.length; i++) {
+        if (response.resultSet.location[i].desc.includes(place.name)) {
+          var stop = response.resultSet.location[i];
+          locationDesc = '<b>' + stop.desc + '</b>';
+          locationDesc += '<br>locID=' + stop.locid;
+          locationDesc += '<br>lat=' + lat;
+          locationDesc += '<br>lng=' + lng;
+          getArrivals(stop, map, marker);
+        }
+      }
+      if (locationDesc === undefined) {
+        locationDesc = 'Oops! Couldn\'t find information for this stop';
+      }
+      infoWindow.setContent(locationDesc);
+      infoWindow.open(map, marker);
+      return locationDesc;
+    }
+  });
+  req.send(null);
+
+  return place.name + '<br>Lat:' + lat + '<br>Lng:' + lng;
+}
+
+function getArrivals(stop, map, marker) {
+  var arrivalCount = 3;
+  var minuteDisplayThreshold = 60;
+  var req = new XMLHttpRequest();
+
+  // Using locid query for arrivals at the stop id using something like:
+  // https://developer.trimet.org/ws/v2/arrivals?appID=188EB70D57290068482857DDD&locIDs=7777&arrivals=5&json=true&begin=2016-05-19T06:00:00&end=2016-05-19T09:00:00
+  // The results will be in the resultSet.arrival[]. Group results by shortSign and convert scheduled time in ms since epoch to time format.
+  // When quering this api, arrivals will be number of arrival times for each type of route. begin and end time should be set to now and sometime 12 or more hours out to ensure data always comes back.
+  // If end time is left out, it will auto set to 1 hour after begin.
+  req.open('GET', buildArrivalsUrl(stop.locid, arrivalCount), true);
+
+  req.addEventListener('load', function() {
+    if (req.status >= 200 && req.status < 400) {
+      var response = JSON.parse(req.responseText);
+      var arrivals = response.resultSet.arrival;
+      var nowTime = new Date().getTime();
+      var timeDiff;
+      var locationDesc = '';
+      var arrivalMap = {};
+      for (var i = 0; i < arrivals.length; i++) {
+        if (!(arrivals[i].shortSign in arrivalMap)) {
+          arrivalMap[arrivals[i].shortSign] = [];
+        }
+        if (arrivalMap[arrivals[i].shortSign].length < arrivalCount) {
+          timeDiff = arrivals[i].scheduled - nowTime;
+          timeDiff = Math.floor(timeDiff / 1000 / 60);
+          if (timeDiff <= minuteDisplayThreshold) {
+            arrivalMap[arrivals[i].shortSign].push(timeDiff + ' minutes');
+          } else {
+            var d = new Date(0);
+            d.setUTCMilliseconds(arrivals[i].scheduled);
+            arrivalMap[arrivals[i].shortSign].push(padNum(d.getHours(), 2) + ':' + padNum(d.getMinutes(), 2));
+          }
+        }
+      }
+      for (var key in arrivalMap) {
+        locationDesc += '<br><b>' + key + '</b>';
+        for (var i = 0; i < arrivalMap[key].length; i++) {
+          locationDesc += '<br>' + arrivalMap[key][i];
+        }
+      }
+      infoWindow.setContent(infoWindow.getContent() + '<br>' + locationDesc);
+      infoWindow.open(map, marker);
+    }
+  });
+  req.send(null);
+}
+
+function buildStopsUrl(lat, lng, stopSearchRadius) {
+  var url = 'https://developer.trimet.org/ws/V1/stops?';
+  url += trimetParams();
+  url += '&ll=' + lng + ',' + lat;
+  url += '&meters=' + stopSearchRadius;
+  return url;
+}
+
+function buildArrivalsUrl(locid, numResults) {
+  var begin = Math.floor(((new Date().getTime()) / 1000));
+  var url = 'https://developer.trimet.org/ws/v2/arrivals?';
+  url += trimetParams();
+  url += '&locIDs=' + locid;
+  url += '&arrivals=' + numResults;
+  url += '&begin=' + begin;
+  url += '&end=' + begin + (60 * 60 * 8);
+  return url;
+}
+
+function trimetParams() {
+  return 'appID=188EB70D57290068482857DDD&json=true'
+}
+
+function padNum(value, totalSize) {
+  var result = value + '';
+  while (result.length < totalSize) {
+    result = '0' + result;
+  }
+  return result;
 }
